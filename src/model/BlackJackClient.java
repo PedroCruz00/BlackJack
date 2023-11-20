f04845dddd83bc6b7d2199bc4c18a5c82e915683
SebastianZarate <juzaor2003@gmail.com>
2023-11-20 02:15:41 -0500

    Se añadio las cartas del cliente

--- src/model/BlackJackClient.java
+++ src/model/BlackJackClient.java
@@ -9,47 +9,43 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class BlackJackClient extends JFrame {
-    private String serverIp ;
+    private String serverIp;
     private int serverPort;
     private Socket socket;
     private JButton hit;
     private JButton stay;
     private JPanel buttons;
     private JTextArea displayArea;
-    private String message ;
+    private String message;
     private ObjectInputStream input;
     private ObjectOutputStream output;
     private boolean inGame;
 
     public BlackJackClient(String ip, String port) {
-        super( "Player" );
+        super("Player");
         this.serverIp = ip;
         this.serverPort = Integer.parseInt(port);
         inGame = true;
-        this.message =  "";
+        this.message = "";
         buttons = new JPanel();
-        buttons.setLayout(new GridLayout(1,2));
+        buttons.setLayout(new GridLayout(1, 2));
         hit = new JButton("Hit");
         stay = new JButton("Stay");
         hit.addActionListener(
 
-                new ActionListener()
-                {
+                new ActionListener() {
                     @Override
-                    public void actionPerformed( ActionEvent event )
-                    {
-                        sendData( "hit" );
+                    public void actionPerformed(ActionEvent event) {
+                        sendData("hit");
                     }
                 }
         );
 
         stay.addActionListener(
-                new ActionListener()
-                {
+                new ActionListener() {
                     // send message to server
-                    public void actionPerformed( ActionEvent event )
-                    {
-                        sendData( "stay" );
+                    public void actionPerformed(ActionEvent event) {
+                        sendData("stay");
                     }
                 }
         );
@@ -57,123 +53,111 @@ public class BlackJackClient extends JFrame {
         buttons.add(hit, BorderLayout.SOUTH);
         buttons.add(stay, BorderLayout.SOUTH);
         buttons.setVisible(true);
-        add(buttons,BorderLayout.SOUTH);
+        add(buttons, BorderLayout.SOUTH);
         displayArea = new JTextArea(); // create displayArea
-        add( new JScrollPane( displayArea ), BorderLayout.CENTER );
+        add(new JScrollPane(displayArea), BorderLayout.CENTER);
 
-        setSize( 300, 300 ); // set size of window
-        setVisible( true ); // show window
+        setSize(300, 300); // set size of window
+        setVisible(true); // show window
         try {
             socket = new Socket(serverIp, serverPort);
             System.out.println("Conectado al servidor: " + socket);
 
             input = new ObjectInputStream(socket.getInputStream());
-            output = new ObjectOutputStream( socket.getOutputStream());
+            output = new ObjectOutputStream(socket.getOutputStream());
             output.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void connectToServer() throws IOException
-    {
-        displayMessage( "Attempting connection\n" );
+    private void connectToServer() throws IOException {
+        displayMessage("Attempting connection\n");
 
-        socket = new Socket( serverIp, serverPort );
+        socket = new Socket(serverIp, serverPort);
 
-        displayMessage( "Connected to: " +
-                socket.getInetAddress().getHostName() );
+        displayMessage("Connected to: " +
+                socket.getInetAddress().getHostName());
     }
 
-    private void getStreams() throws IOException
-    {
+    private void getStreams() throws IOException {
         // set up output stream for objects
-        output = new ObjectOutputStream( socket.getOutputStream() );
+        output = new ObjectOutputStream(socket.getOutputStream());
         output.flush(); // flush output buffer to send header information
 
         // set up input stream for objects
-        input = new ObjectInputStream( socket.getInputStream() );
-        displayMessage( "\nGot I/O streams\n" );
+        input = new ObjectInputStream(socket.getInputStream());
+        displayMessage("\nGot I/O streams\n");
     } // en
-    private void processConnection() throws IOException
-    {
-        do
-        {
-            try
-            {
-                message = ( String ) input.readObject();
-                displayMessage( "\n" + message );
-                if (message.contains("Bust!") || message.contains("Please Wait")){
+
+    private void processConnection() throws IOException {
+        do {
+            try {
+                message = (String) input.readObject();
+                displayMessage("\n" + message);
+                if (message.contains("Bust!") || message.contains("Please Wait")) {
                     buttons.setVisible(false);
                 }
 
             } // end try
-            catch ( ClassNotFoundException classNotFoundException )
-            {
-                displayMessage( "\nUnknown object type received" );
+            catch (ClassNotFoundException classNotFoundException) {
+                displayMessage("\nUnknown object type received");
             } // end catch
 
-        } while ( !message.equals( "SERVER>>> TERMINATE" ) );
+        } while (!message.equals("SERVER>>> TERMINATE"));
     }
 
     public void closeConnection() {
-                try {
-                    input.close();
-                    output.close();
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-    public void exit(String response){
+        try {
+            input.close();
+            output.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void exit(String response) {
         if (response.equalsIgnoreCase("Hasta luego")) {
             inGame = false;
         }
     }
-    public void sendData( String message )
-    {
-        try
-        {
-            output.writeObject(  message );
+
+    public void sendData(String message) {
+        try {
+            output.writeObject(message);
             output.flush();
 
         } // end try
-        catch ( IOException ioException ) {
+        catch (IOException ioException) {
 
         }
     }
-    public void runClient()
-    {
-        try
-        {
+
+    public void runClient() {
+        try {
             connectToServer();
             getStreams();
             processConnection();
-        }
-        catch ( EOFException eofException )
-        {
-            displayMessage( "\nClient terminated connection" );
-        }
-        catch ( IOException ioException )
-        {}
-        finally
-        {
+        } catch (EOFException eofException) {
+            displayMessage("\nClient terminated connection");
+        } catch (IOException ioException) {
+        } finally {
             closeConnection();
         }
     }
-    private void displayMessage( final String messageToDisplay )
-    {
+
+    private void displayMessage(final String messageToDisplay) {
         SwingUtilities.invokeLater(
-                new Runnable()
-                {
-                    public void run()
-                    {
-                        displayArea.append( messageToDisplay );
+                new Runnable() {
+                    public void run() {
+                        displayArea.append(messageToDisplay);
                     }
                 }
         );
     }
-    public boolean getInGame(){
+
+    public boolean getInGame() {
         return inGame;
     }
 }
--- src/model/Dealer.java
+++ src/model/Dealer.java
@@ -24,6 +24,7 @@ public class Dealer extends JFrame {
     private Player dealer;
     private boolean roundOver;
 
+
     public Dealer() {
         super("Dealer");
 
@@ -79,12 +80,14 @@ public class Dealer extends JFrame {
         sockServers.add(sockServer);
         executor.execute(sockServer);
     }
+
     public void playerStay(int playerIndex) {
         if (!roundOver) {
             players.get(playerIndex).setPlayerState("Stay");
             checkRoundOver();
         }
     }
+
     private void checkRoundOver() {
         boolean allPlayersStayed = players.stream().allMatch(player -> player.getPlayerState().equals("Stay"));
         if (allPlayersStayed) {
@@ -95,6 +98,7 @@ public class Dealer extends JFrame {
             displayResults();
         }
     }
+
     public void displayResults() {
         for (SockServer sockServer : sockServers) {
             Player player = sockServer.getPlayer();
@@ -126,6 +130,14 @@ public class Dealer extends JFrame {
             dealer.resetHand();
             dealCards();
             displayMessage("\n\nCARDS DEALT\n\n");
+
+            drawPlayerCards();
+        }
+    }
+
+    private void drawPlayerCards() {
+        for (SockServer sockServer : sockServers) {
+            sockServer.drawPlayerCards();
         }
     }
 
@@ -148,10 +160,16 @@ public class Dealer extends JFrame {
         player.addCardToHand(deck.dealCard());
         player.addCardToHand(deck.dealCard());
 
-        sockServer.sendData("You were dealt: " + player.getHand().get(0).toString() +
-                " and " + player.getHand().get(1).toString() + "\n");
+        sockServer.sendData("You were dealt: " + player.getHand().get(0) +
+                " and " + player.getHand().get(1) + "\n");
+
 
         sockServer.sendData("Your total: " + player.getHandValue() + "\n");
+
+        for (Card card : player.getHand()) {
+            CardComponent cardComponent = new CardComponent(card);
+            sockServer.playerCardComponents.add(cardComponent);
+        }
     }
 
     public void playerHit(SockServer sockServer) {
@@ -229,9 +247,10 @@ public class Dealer extends JFrame {
         SwingUtilities.invokeLater(() -> displayArea.append(messageToDisplay));
     }
 
-    public List<SockServer> getSockServers(){
+    public List<SockServer> getSockServers() {
         return sockServers;
     }
+
     public ServerSocket getServer() {
         return server;
     }
@@ -247,9 +266,11 @@ public class Dealer extends JFrame {
         private Socket connection;
         private Dealer dealer;
         private Player player;
+        private List<CardComponent> playerCardComponents;
 
 
         public SockServer(Socket connection, Dealer dealer) {
+            this.playerCardComponents = new ArrayList<>();
             this.connection = connection;
             this.dealer = dealer;
             this.player = new Player("Player" + dealer.getSockServers().size());
@@ -266,6 +287,21 @@ public class Dealer extends JFrame {
             }
         }
 
+        public void drawPlayerCards() {
+            // Crea un JFrame para mostrar las cartas del jugador
+            JFrame playerFrame = new JFrame("Player: " + player.getName());
+            playerFrame.setSize(300, 150);
+            playerFrame.setLayout(new FlowLayout());
+
+            // Agrega las cartas del jugador al JFrame
+            for (CardComponent cardComponent : playerCardComponents) {
+                playerFrame.getContentPane().add(cardComponent);
+            }
+
+            // Muestra el JFrame
+            playerFrame.setVisible(true);
+        }
+
         private void getStreams() throws IOException {
             output = new ObjectOutputStream(connection.getOutputStream());
             output.flush();
@@ -280,14 +316,15 @@ public class Dealer extends JFrame {
 
             do {
                 try {
-                    String message = (String) input.readObject();
-                    dealer.displayMessage("Received: " + message + "\n");
+                    Card card = (Card) input.readObject();
+                    dealer.displayMessage("Received: " + card + "\n");
 
-                    if (message.equals("hit")) {
+                    if (card.equals("hit")) {
                         dealer.playerHit(this);
-                    } else if (message.equals("stay")) {
+                    } else if (card.equals("stay")) {
                         dealer.playerStand(this);
                     }
+
 
                 } catch (ClassNotFoundException e) {
                     dealer.displayMessage("Unknown object type received\n");
@@ -324,5 +361,6 @@ public class Dealer extends JFrame {
             return player;
         }
     }
+
 }
 
